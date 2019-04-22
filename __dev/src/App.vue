e3bc23646f36980f16acb9bcca61e0bc8ed812f9
Volkmar Kantor <volkmar.kantor@gmx.de>
2019-04-22 22:41:43 +0200

    reload, buttons

--- __dev/src/App.vue
+++ __dev/src/App.vue
@@ -11,6 +11,7 @@
 
             <v-footer app class="pa-3">
                 <v-spacer></v-spacer>
+                <div><v-icon @click="doReload">autorenew</v-icon></div>
                 <div class="versionInfo"><small><span class="text">Version:</span> <span v-html="versionDate"></span></small></div>
             </v-footer>
         </div>
@@ -29,6 +30,11 @@ export default {
                 value = '<span class="date">'+ ('0'+value.getDate()).slice(-2)+'.'+('0'+(value.getMonth()+1)).slice(-2)+'.'+value.getFullYear()+'</span> <span class="time">'+('0'+value.getHours()).slice(-2)+':'+('0'+value.getMinutes()).slice(-2)+'</span>';
             }
             return value;
+        }
+    },
+    methods:{
+        doReload() {
+            location.reload();
         }
     }
 }
